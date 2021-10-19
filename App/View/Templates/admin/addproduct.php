69efd677df6afa99510b1710c190fba413232311
Boris1310 <novyjpolzovatel@MacBook-Pro-Boris.local>
2021-10-19 16:01:51 +0300

    Пишу пагинацию для страничек категорий

---
+++ App/View/Templates/admin/addproduct.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_SESSION['success'])){
+    echo '<div class="alert alert-success">'.$_SESSION['success'].'</div>';
+}
+?>
+
+<h3>Страница добавления товара</h3>
+
+<form action="http://localhost:8888/catalog/setProduct" class="text-center center-block" method="post">
+    <div class="my-3">
+        <label for="name" class="form-label">Наименование</label>
+        <input type="text" name="name" id="name" placeholder="Наименование" class="form-control w-50 mx-auto" required>
+        <br>
+        <label for="description">Описание</label>
+        <textarea name="description" id="description" class="form-control w-50 mx-auto" required></textarea>
+        <br>
+        <label for="price">Цена</label>
+        <input type="number" id="price" step="0.01" name="price" class="form-control w-50 mx-auto" required>
+        <br>
+        <label for="category">Категория</label>
+        <select name="category" id="category" class="form-control w-50 mx-auto" required>
+            <?php
+            echo "<option value=''>Выберите категорию...</option>";
+            foreach ($data as $cat) {
+                echo "<option value='" . $cat['idCategory'] . "'>" . $cat['cat_name'] . "</option>";
+            }
+            ?>
+        </select>
+        <br>
+        <label for="manufacturer">Производитель</label>
+        <select name="manufacturer" id="manufacturer" class="form-control w-50 mx-auto" required>
+            <?php
+            echo "<option value=''>Выберите производителя...</option>";
+            foreach ($data2 as $man) {
+                echo "<option value='" . $man['idmanufacturer'] . "'>" . $man['name'] . "</option>";
+            }
+            ?>
+        </select>
+        <br>
+        <input type="submit" class="form-control btn btn-primary w-50 mx-auto" value="Добавить товар">
+    </div>
+</form>
